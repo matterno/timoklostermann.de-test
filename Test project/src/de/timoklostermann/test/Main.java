7c4cb7697775da9b12b01d75d36df6cd42c7d98f
matterno <timo.klostermann@gmail.com>
2012-02-03 22:00:10 +0100

    First commit! :)

---
+++ Test project/src/de/timoklostermann/test/Main.java
@@ -0,0 +1,18 @@
+package de.timoklostermann.test;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		doSomething();
+	}
+	
+	private static void doSomething() {
+		for(int i = 0; i<10; i++) {
+			System.out.println("bla");
+		}
+	}
+
+}
